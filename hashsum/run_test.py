a5617fec8978447d3651b8947f8413047aa3a1c8
Antonio Valentino <antonio.valentino@tiscali.it>
2016-09-10 09:39:44 +0200

    Adopt conda-forge template

--- hashsum/run_test.py
+++ hashsum/run_test.py
@@ -10,11 +10,6 @@ import hashsum
 print('hashsum: v%s' % hashsum.VERSION)
 print('Python: %s' % sys.version)
 
-shutil.copytree(os.path.join(os.environ['SRC_DIR'], 'tests'), 'tests')
-
 from tests.test_hashsum import *
 
-try:
-    unittest.main(verbosity=2)
-finally:
-    shutil.rmtree('tests')
+unittest.main(verbosity=2)
--- pyepr/run_test.py
+++ pyepr/run_test.py
@@ -11,12 +11,11 @@ print('PyEPR: %s' % epr.__version__)
 print('EPR API: %s' % epr.EPR_C_API_VERSION)
 print('Python: %s' % sys.version)
 
-shutil.copy(os.path.join(os.environ['SRC_DIR'], 'tests', 'test_all.py'), '.')
+sys.path.append('tests')
 
 from test_all import *
 
 try:
     unittest.main(verbosity=2)
 finally:
-    os.remove(TEST_PRODUCT)
-    os.remove('test_all.py')
+    os.remove(os.path.join(TESTDIR, TEST_PRODUCT))
